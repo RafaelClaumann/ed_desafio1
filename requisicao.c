9ee04b3b1f60300f007adfbcf81931f239759464
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-09-17 15:05:02 -0300

    cria, destroi e melhorias requisicao

--- requisicao.c
+++ requisicao.c
@@ -1,13 +1,41 @@
 #include "requisicao.h"
+#include <stdlib.h>
+#include <string.h>
 
-char *get_nome(Requisicao *req) { 
-    return req->nome;
+Requisicao *cria_requisicao(char *nome_, int inscricao_, char *procedimento_) {
+  if (!nome_ || !procedimento_)
+    return NULL;
+
+  Requisicao *r = malloc(sizeof(Requisicao));
+  if (!r)
+    return NULL;
+
+  strncpy(r->nome, nome_, sizeof(r->nome) - 1);
+  r->nome[sizeof(r->nome) - 1] = '\0';
+
+  r->codigo_inscricao = inscricao_;
+
+  strncpy(r->codigo_procedimento, procedimento_, sizeof(r->codigo_procedimento) - 1);
+  r->codigo_procedimento[sizeof(r->codigo_procedimento) - 1] = '\0';
+
+  return r;
 }
 
-int *get_inscricao(Requisicao *req) { 
-    return &req -> codigo_inscricao;
+void destroi_requisicao(Requisicao *r) {
+  free(r);
 }
 
-char *get_procedimento(Requisicao *req) { 
-    return req->codigo_procedimento;
+char *get_nome(Requisicao *req) {
+  if (!req) return NULL;
+  return req->nome;
+}
+
+int *get_inscricao(Requisicao *req) {
+  if (!req) return NULL;
+  return &req->codigo_inscricao;
+}
+
+char *get_procedimento(Requisicao *req) {
+  if (!req) return NULL;
+  return req->codigo_procedimento;
 }
--- requisicao.h
+++ requisicao.h
@@ -1,11 +1,18 @@
 #ifndef REQUISICAO_H
 #define REQUISICAO_H
 
+#define REQ_TAMANHO_NOME 40
+#define REQ_TAMANHO_PROCEDIMENTO 10
+
 typedef struct {
-    char nome[40];
+    char nome[REQ_TAMANHO_NOME];
     int codigo_inscricao;
-    char codigo_procedimento[10];
+    char codigo_procedimento[REQ_TAMANHO_PROCEDIMENTO];
 } Requisicao;
+
+Requisicao *cria_requisicao(char *nome, int codigo_inscricao, char *codigo_procedimento);
+
+void destroi_requisicao(Requisicao *r);
 
 char *get_nome(Requisicao *req);
 
