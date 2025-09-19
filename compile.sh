5c945d3ce69c387797091319cf882797d618801f
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-09-19 09:26:43 -0300

    adicion Wall e Wextra no GCC compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-gcc ./src/teste.c ./src/requisicao.c ./src/estrutura.c -o output && ./output
+gcc -Wall -Wextra ./src/teste.c ./src/requisicao.c ./src/estrutura.c -o output && ./output
 rm -r output
