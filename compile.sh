40d99ea401257372ba4e0233a7caf40d20f1ec65
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-09-20 09:56:56 -0300

    melhoria compile.sh para diferenciar arquivos

--- compile.sh
+++ compile.sh
@@ -1,2 +1,11 @@
-gcc -Wall -Wextra ./test/teste_estrutura.c ./src/requisicao.c ./src/estrutura.c -o output && ./output
+#!/bin/sh
+
+if [ "$1" = "x" ]; then
+    echo -e "Usando arquivo test_estrutura.c \n"
+    gcc -Wall -Wextra ./test/teste_estrutura.c ./src/requisicao.c ./src/estrutura.c -o output && ./output
+else
+    echo -e "Usando arquivo test_senai.c \n"
+    gcc -Wall -Wextra ./test/teste_senai.c ./src/requisicao.c ./src/estrutura.c -o output && ./output
+fi
+
 rm -r output
